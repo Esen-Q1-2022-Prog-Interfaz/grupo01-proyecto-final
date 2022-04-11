f803c034ec9004f5f59e631dcfcc08e1a993d924
Monterrosar105 <20205751@esen.edu.sv>
2022-04-11 11:44:25 -0600

    catalogo form y bd

---
+++ forms/catalogoform.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, FloatField
+from wtforms.validators import InputRequired, Length, ValidationError
+
+
+class CatalogoForm(FlaskForm):
+    sabor = StringField(
+        validators=[InputRequired(), Length(max = 50)],
+        render_kw={'placeholder':'Sabor'})
+
+    descripcion = StringField(
+        validators=[InputRequired(), Length(max = 50)],
+        render_kw={'placeholder':'Descripción'})
+
+    base = StringField(
+        validators=[InputRequired(), Length(max = 50)],
+        render_kw={'placeholder':'Base'})
+
+    tamaño = IntegerField(
+        validators=[InputRequired()],
+        render_kw={'placeholder':'Tamaño'})
+
+    nic = FloatField(
+        validators=[InputRequired()],
+        render_kw={'placeholder':'Tamaño'})
+    
+    stock = IntegerField(
+        validators=[InputRequired()],
+        render_kw={'placeholder':'Stock'})
--- models/catalogo.py
+++ models/catalogo.py
@@ -4,18 +4,18 @@ class catalogo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sabor = db.Column(db.String(50), nullable=False)
     descripcion = db.Column(db.String(50), nullable=False)
-    tipo = db.Column(db.String(50), nullable=False)
+    base = db.Column(db.String(50), nullable=False)
     tamaño = db.Column(db.Integer, nullable=False)
-    nic = db.Column(db.Integer, nullable=True)
+    nic = db.Column(db.Float, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, sabor, descripcion, tipo, tamaño, nic, stock) -> None:
+    def __init__(self, sabor, descripcion, base, tamaño, nic, stock) -> None:
         self.sabor = sabor
         self.descripcion = descripcion
-        self.tipo = tipo
+        self.base = base
         self.tamaño = tamaño
         self.nic = nic
         self.stock = stock
 
     def __repr__(self) -> str:
-        return f"Order({self.id}, {self.sabor}, '{self.descripcion}', '{self.nic}', '{self.tipo}', '{self.stock}', '{self.tamaño}')"
+        return f"Order({self.id}, {self.sabor}, '{self.descripcion}', '{self.nic}', '{self.base}', '{self.stock}', '{self.tamaño}')"
