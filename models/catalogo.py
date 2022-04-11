9400ee57ba2909c082f8eb853fe8ac390b0488bc
Monterrosar105 <20205751@esen.edu.sv>
2022-04-11 13:57:54 -0600

    crud catalogo

--- models/catalogo.py
+++ models/catalogo.py
@@ -1,6 +1,6 @@
 from db.db import db
 
-class catalogo(db.Model):
+class Catalogo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sabor = db.Column(db.String(50), nullable=False)
     descripcion = db.Column(db.String(50), nullable=False)
--- routes/admin.py
+++ routes/admin.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user, login_required
+from forms.catalogoform import CatalogoForm
+from models.catalogo import Catalogo
 from db.db import db
 
 
@@ -8,3 +11,18 @@ admin = Blueprint("admin", __name__, url_prefix='/admin')
 @admin.route("/")
 def home():
     return render_template("admin/dashboard.html")
+
+@admin.route("/CRUD_Catalogo", methods=["GET", "POST"])
+@login_required
+def create():
+    form = CatalogoForm()
+    if form.validate_on_submit():
+        comprador = form.comprador.data
+        vendedor = form.vendedor.data
+        impuesto = form.impuesto.data
+        descuento = form.descuento.data
+        newOrder = Order(comprador, vendedor, impuesto, descuento)
+        db.session.add(newOrder)
+        db.session.commit()
+        return redirect(url_for("orders.home"))
+    return render_template("orders/create.html", form=form)
